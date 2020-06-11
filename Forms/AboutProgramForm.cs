2ac802c5592b8e68046f75fc70c53f486069c735
Dominik Zakrzewski <dominikzakrzewski99@gmail.com>
2020-06-11 17:56:12 +0200

    dodanie pomocy

---
+++ Forms/AboutProgramForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinSMSer.Properties;
+
+namespace WinSMSer.Forms
+{
+    public partial class AboutProgramForm : Form
+    {
+        public AboutProgramForm()
+        {
+            InitializeComponent();
+            webBrowser.DocumentText = "";
+        }
+    }
+}
