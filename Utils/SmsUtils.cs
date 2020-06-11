568d4256b31d3f75383f3b31e700eb78f04936ee
Dominik Zakrzewski <dominikzakrzewski99@gmail.com>
2020-06-11 17:56:30 +0200

    funkcje pomocnicze; walidacja numeru telefonu

---
+++ Utils/SmsUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WinSMSer.Utils
+{
+    public static class SmsUtils
+    {
+        public static bool validatePhoneNumer(string phoneNumber)
+        {
+            Regex reg = new Regex(@"^[\d]{9}$", RegexOptions.IgnoreCase);
+            Match m = reg.Match(phoneNumber);
+
+            return m.Success;
+        }
+    }
+}
